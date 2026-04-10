de294d2b4842d98ef514c49c3ea761d730be906d
HuMaNus@s <73073973+HuMaNus2s@users.noreply.github.com>
2026-04-10 09:33:53 +0300

    Update config.py
    Signed-off-by: HuMaNus@s <73073973+HuMaNus2s@users.noreply.github.com>

--- config/config.py
+++ config/config.py
@@ -8,11 +8,4 @@ class web:
     SERVER_PORT = 5000
     SECRET_KEY = 'vfyh473g-f387x2cg-63tfr34u438'
 
-ON_RENDER = os.environ.get('RENDER') == 'true'
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-if ON_RENDER:
-    DATA_PATH = BASE_DIR / 'data'
-else:
-    DATA_PATH = BASE_DIR / 'data'
+DATA_PATH = "/opt/render/project/src/data"
