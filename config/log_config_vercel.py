f3e3ab23939771eabc8dfb1a900600cafeb2c414
HuMaNus@s <73073973+HuMaNus2s@users.noreply.github.com>
2026-04-10 09:13:33 +0300

    add

--- config/log_config_vercel.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logger = logging.getLogger('test_system')
-logger.setLevel(logging.INFO)
-
-# Отключен для Vercel
--- run.py
+++
@@ -1,8 +0,0 @@
-from web.app import app
-from config.config import web
-from managers.CategoryManager import CategoryManager
-
-if __name__ == '__main__':
-    manager = CategoryManager()
-    manager.load_all_categories()
-    app.run(debug=False, host='0.0.0.0', port=web.SERVER_PORT)
